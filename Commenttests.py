8ad3bcda5ef5123c44b7fbe86dd6989e4c8e4b45
hardwareandme <ravi.ranjan0980@gmail.com>
2017-10-29 16:08:08 +0530

    Fixed comment by user2

--- Commenttests.py
+++ Commenttests.py
@@ -34,7 +34,7 @@ try:
     comment_section = browser.find_element_by_xpath(".//*[@id='comment']")
     comment_section.clear()
     #add comment
-    comment_section.send_keys("Comment by user 1")
+    comment_section.send_keys("Comment by user 1bcd")
     #find email field
     email = browser.find_element_by_xpath(".//*[@id='email']")
     #add email
@@ -63,7 +63,7 @@ try:
     comment = browser.find_element_by_id("comment")
     comment.clear()
     #add comment
-    comment.send_keys("Comment by user2")
+    comment.send_keys("Comment by user2namenb")
     user2name = browser.find_element_by_name("author")
     #clear username field
     user2name.clear()
@@ -74,11 +74,13 @@ try:
     user2email.clear()
     #add email
     user2email.send_keys("user2@typeset.io")
-    submit_comment = browser.find_element_by_name("submit")
+    submit_comment = browser.find_element_by_class_name("submit")
     submit_comment.click()
-    time.sleep(30)
-    assert "comment by User2" in browser.page_source
+    time.sleep(10)
+    assert "User 2" in browser.page_source
     print "comment 2 is added as a reply to comment 1"
+    time.sleep(5)
 except:
     print "Reply to comment1 not added"
     raise Exception(browser.quit())
+browser.quit()
